4c8e7cecb7a9ab732ab6e4393ce50127f0bf9cb1
Andrew D Bechdolt <abechdolt@nevada.unr.edu>
2019-11-05 19:18:01 -0800

    Does this work?

---
+++ main.c
@@ -0,0 +1,16 @@
+//Name: Main.c
+//Purpose: Who cares, I don't know
+//Author: Andrew Bechdolt
+//Date: November 2019
+
+#include<stdio.h>
+
+float math_function(float x)
+{
+  return 0;
+}
+
+int main()
+{
+  return 0;
+}
